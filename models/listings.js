84892ae1005ba3c80845025a79e3cface94fbb55
akash more <akashmore411028@gamil.com>
2024-08-23 23:49:33 +0530

    Initial commit

---
+++ models/listings.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// const { string } = require("joi");
+// const review = require("./review");
+const Review = require("./review.js");
+// const { listingSchema } = require("../schema");
+
+
+let ListingSchema = new  Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+
+    description:String,
+
+    image:{
+    filename:String,
+       url:String,
+    
+    },
+
+    price:Number,
+    location:String,
+    country:String,
+
+reviews:[
+    {
+        type:Schema.Types.ObjectId,
+        ref:"Review"
+    }
+],
+
+owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+}
+
+});
+
+
+ListingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: {$in:listing.reviews}});
+    }
+})
+
+
+const Listing = mongoose.model("Listing",ListingSchema);
+module.exports =Listing;
